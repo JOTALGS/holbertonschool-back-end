68d8e34a6e49f5f3148ac44511befb68e59513a7
jotalgs <jose.pgilsuarez@gmail.com>
2023-12-19 18:34:36 +0000

    k

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,40 +4,31 @@ import requests
 import sys
 
 
-task_data = requests.get('https://jsonplaceholder.typicode.com/todos')
-empl_data = requests.get('https://jsonplaceholder.typicode.com/users')
+if __name__ == '__main__':
+    task_data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    empl_data = requests.get('https://jsonplaceholder.typicode.com/users')
 
-task_str = task_data.text.replace('true', 'True').replace('false', 'False')
-empl_str = empl_data.text.replace('true', 'True').replace('false', 'False')
+    task_str = task_data.text.replace('true', 'True').replace('false', 'False')
+    empl_str = empl_data.text.replace('true', 'True').replace('false', 'False')
 
-task_list = eval(task_str)
-empl_list = eval(empl_str)
+    task_list = eval(task_str)
+    empl_list = eval(empl_str)
 
-total_tasks = {}
-done_tasks = {}
-empl_dict = {}
+    total_tasks = {}
+    done_tasks = {}
+    empl_dict = {}
 
-for tasks in task_list:
-    user_id = tasks['userId']
-    total_tasks[user_id] = total_tasks.get(user_id, 0) + 1
-    if tasks['completed']:
-        done_tasks[user_id] = done_tasks.get(user_id, 0) + 1
+    for tasks in task_list:
+        user_id = tasks['userId']
+        total_tasks[user_id] = total_tasks.get(user_id, 0) + 1
+        if tasks['completed']:
+            done_tasks[user_id] = done_tasks.get(user_id, 0) + 1
 
-for employee in empl_list:
-    empl_dict[employee['id']] = employee['name']
+    for employee in empl_list:
+        empl_dict[employee['id']] = employee['name']
 
-if len(sys.argv) > 1:
-    key = int(sys.argv[1])
-    str1 = f'Employee {empl_dict[key]} is done with '
-    str2 = f'({done_tasks[key]}/{total_tasks[key]}) tasks:'
-    str_fin = str1 + str2 + '\n'
-    for task in task_list:
-        if task['completed'] and task['userId'] == key:
-            str_fin += '\t ' + task['title'] + '\n'
-    str_fin = str_fin[:-1]
-    print(str_fin)
-else:
-    for key in done_tasks.keys():
+    if len(sys.argv) > 1:
+        key = int(sys.argv[1])
         str1 = f'Employee {empl_dict[key]} is done with '
         str2 = f'({done_tasks[key]}/{total_tasks[key]}) tasks:'
         str_fin = str1 + str2 + '\n'
@@ -46,3 +37,13 @@ else:
                 str_fin += '\t ' + task['title'] + '\n'
         str_fin = str_fin[:-1]
         print(str_fin)
+    else:
+        for key in done_tasks.keys():
+            str1 = f'Employee {empl_dict[key]} is done with '
+            str2 = f'({done_tasks[key]}/{total_tasks[key]}) tasks:'
+            str_fin = str1 + str2 + '\n'
+            for task in task_list:
+                if task['completed'] and task['userId'] == key:
+                    str_fin += '\t ' + task['title'] + '\n'
+            str_fin = str_fin[:-1]
+            print(str_fin)
