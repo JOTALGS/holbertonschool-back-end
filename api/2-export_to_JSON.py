90aca4d4b4ecbd76e03e7318b0611ceb4df515b1
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-01-30 15:32:55 +0000

    12

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python3
 """
-sdadsd sadsad asdsa
+Created on Mon Apr 3 10:16:00 2023.
+
+@authors: jgnacio
+@description:
+    This script makes a request on jsonplaceholder which returns a user
+    with completed tasks and filter by the id given in the execution,
+    and returns a list of tasks completed in json format.
 """
 import json
-import requests
-from sys import argv
+import sys
+import urllib.request
+
+USER = sys.argv[1]
+COMPLETED = TOTAL_TASK = 0
+TODOS_URL = f'https://jsonplaceholder.typicode.com/todos?userId={USER}'
+EMPLOYEE_URL = f'https://jsonplaceholder.typicode.com/users/{USER}'
+
+# Request for the taks
+with urllib.request.urlopen(TODOS_URL) as response:
+    todos_data = response.read().decode()
+
+# Request for the employee info
+with urllib.request.urlopen(EMPLOYEE_URL) as response:
+    employee_data = response.read().decode()
+
+employee_info = {}
+try:
+    employee_taks = json.loads(todos_data)
+    employee_info = json.loads(employee_data)
+except json.JSONDecodeError:
+    print('Response could not be serialized')
 
 
-def employee_progress(employee_id):
-    """ sadsdsadsd sd sd"""
-    URL = "https://jsonplaceholder.typicode.com"
-    user = requests.get(f'{URL}/users/{employee_id}').json()
-    username = user["username"]
-    todo_list = requests.get(f'{URL}/users/{employee_id}/todos').json()
+# Get all the taks name in order and how many are complete.
+tasks_list = []
+for task in employee_taks:
+    tasks_list.append({
+        'task': task['title'],
+        "completed": task['completed'],
+        "username": employee_info['name']}
+    )
 
-    filename = f"{employee_id}.json"
-    with open(filename, "w") as f:
-        progress = {f"{employee_id}": []}
-        for task in todo_list:
-            task_dict = {}
-            task_dict["task"] = task["title"]
-            task_dict["completed"] = task["completed"]
-            task_dict["username"] = username
-            progress[f"{employee_id}"].append(task_dict)
-
-        f.write(json.dumps(progress))
-
-
-if __name__ == "__main__":
-    employee_progress(argv[1])
+with open(f'{USER}.json', 'w', encoding='utf8') as f:
+    f.write(json.dumps({str(USER): tasks_list}, indent=4))
